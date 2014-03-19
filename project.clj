b9ed6e79c2078680b5e830a7550166f3f29dcfdc
Nicolas Berger <nicoberger@gmail.com>
2014-03-18 21:14:32 -0700

    Use joda-time 2.3, not 2.4
    I couldn't get joda-time 2.4

--- project.clj
+++ project.clj
@@ -7,6 +7,6 @@
                  [org.igniterealtime.smack/smack "3.4.1"]
                  [org.igniterealtime.smack/smackx "3.4.1"]
                  [clj-http "0.9.1"]
-                 [joda-time/joda-time "2.4"]
+                 [joda-time/joda-time "2.3"]
                  [com.draines/postal "1.11.1"]
                  ])
