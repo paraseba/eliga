51fe1a3796954968cc49ea5effd47ae70f79e7cb
Nicolas Berger <nicoberger@gmail.com>
2014-03-22 11:40:52 -0700

    Ack standup update received
    Acks via a private-message. Only acks messages that contain an update

--- src/paraseba/eliga/standup_bot.clj
+++ src/paraseba/eliga/standup_bot.clj
@@ -27,12 +27,21 @@
   (parse-message "@eliga #today everything")
   (parse-message "@eliga #today idk #today everything")
   (parse-message "@eliga #today idk #yesterday everything")
+  (parse-message "@eliga sending an invalid update")
   )
 
+(defn- ack-updates-received
+  [group-chat session to updates]
+  (let [update-titles (map #(str "#" (name %)) (-> updates keys sort reverse))
+        message (str (clojure.string/join " & " update-titles) " update received")]
+    (bot/private-message group-chat session to message)))
+
 (defn- apply-message!
-  [state message]
+  [group-chat session state message]
   (when (:mention? message)
-    (sstate/add-status state (:room message) (:from message) (parse-message (:body message)))))
+    (when-let [updates (parse-message (:body message))]
+      (sstate/add-status state (:room message) (:from message) updates)
+      (ack-updates-received group-chat session (:from message) updates))))
 
 (defn- status-ready?
   [[_ status]]
@@ -87,7 +96,7 @@
   (let [state (sstate/empty-memory-standup-state)]
     (bot/connect group-chat config
                 (fn [session message]
-                  (apply-message! state message)
+                  (apply-message! group-chat session state message)
                   (when (standup-ready? users (sstate/standup-as-map state (-> config :rooms first)))
                     ((:on-ready config) session (sstate/standup-as-map state (-> config :rooms first)))
                     (sstate/standup-done state (-> config :rooms first)))))))
